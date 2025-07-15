6981e820a31d105051e3eed3d3c9148c8216c69e
jiwoolee2 <dlwldn1009@naver.com>
2025-07-15 11:44:06 +0900

    ssaaa

--- src/main/java/com/example/evcs/configuration/SecurityConfiguration.java
+++ src/main/java/com/example/evcs/configuration/SecurityConfiguration.java
@@ -121,7 +121,7 @@ public class SecurityConfiguration {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration cfg = new CorsConfiguration();
-        cfg.setAllowedOrigins(List.of("https://kh-evcs.store"));
+        cfg.setAllowedOrigins(List.of("http://kh-evcs.store"));
         cfg.setAllowedMethods(List.of("GET","POST","PUT","PATCH","DELETE","OPTIONS"));
         cfg.setAllowedHeaders(List.of("*"));
   
